92d7b18ae6773cf134539664067303e9553b15a0
Steven Zapata <szapata427@gmail.com>
2018-09-06 14:48:50 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,13 @@
 def hello_t(array)
   i = 0
- 
+  
   while i < array.length
-    yield array[i]
-    i = i + 1
+  yield array[1]
+  i += 1
   end
+
 end
+
 # call your method here!
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
